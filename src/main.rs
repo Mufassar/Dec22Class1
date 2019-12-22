a0ed42e0808e7301dadb1b17e0e30f8e0ede563a
Mufassar <mufassarbaig@hotmail.com>
2019-12-22 09:59:12 +0500

    Struct Method Added

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,23 @@ struct Student
     score:u32
 }
 
+impl Student
+{
+    fn details(&self)
+    {
+        println!("The name of student in method is {} & his score is {}", self.name, self.score);
+    }
+}
+
 fn main() {
     
     println!("Class 22 Dec 2019");
 
-    let student1 = Student { name:String::from("Nouman"), score:70 };
+    let mut student1 = Student { name:"Nouman".to_string(), score:70 };    
 
+    student1.details();// Struct Method Calling
 
-    details(student1);
+    details(student1); // Function Calling
 }
 
 fn details(student: Student)
