4aa774f9b6cdea1dc95d3edffb2ee156baee38bd
Mufassar <mufassarbaig@hotmail.com>
2019-12-22 09:40:52 +0500

    Student Struct & Function Added

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+
+#[derive(Debug)]
+struct Student
+{
+    name:String,
+    score:u32
+}
+
 fn main() {
     
-    println!("Hello, world!");
+    println!("Class 22 Dec 2019");
+
+    let student1 = Student { name:String::from("Nouman"), score:70 };
+
+
+    details(student1);
+}
+
+fn details(student: Student)
+{
+    println!("{:#?}", student);
 }
